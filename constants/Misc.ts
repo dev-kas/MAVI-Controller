1228d54c7184f88c6406a3d2898c2438886da67b
KAS <krishaang_1808@hotmail.com>
2025-12-09 18:50:46 +0530

    fix: update server url

--- constants/Misc.ts
+++ constants/Misc.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://192.168.1.123:5500";
-// export const SERVER_URL = "https://mavi.glitchiethedev.com";
+// export const SERVER_URL = "http://192.168.1.123:5500";
+export const SERVER_URL = "https://mavi.glitchiethedev.com";
